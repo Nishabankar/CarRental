721e66ade844dad03427da82a137a34cbf438ec1
Nisha Bankar <nishab2607@gmail.com>
2025-07-17 15:49:55 +0530

    adding env file and minor changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,10 +12,10 @@ const Footer = () => {
                     Premiun car rental service with a wide selection of luxury and everyday vehicals for all your driving needs.
                 </p>
                 <div className='flex items-center gap-3 mt-6'>
-                        <a href="#"><img src={assets.facebook_logo} classname="w-5 h-5" alt="" /></a>
-                        <a href="#"><img src={assets.instagram_logo} classname="w-5 h-5" alt="" /></a>
-                        <a href="#"><img src={assets.twitter_logo} classname="w-5 h-5" alt="" /></a>
-                        <a href="#"><img src={assets.gmail_logo}  classname="w-5 h-5"alt=""/></a>
+                        <a href="#"><img src={assets.facebook_logo} className="w-5 h-5" alt="" /></a>
+                        <a href="#"><img src={assets.instagram_logo} className="w-5 h-5" alt="" /></a>
+                        <a href="#"><img src={assets.twitter_logo} className="w-5 h-5" alt="" /></a>
+                        <a href="#"><img src={assets.gmail_logo}  className="w-5 h-5"alt=""/></a>
                 </div>
             </div>
 
